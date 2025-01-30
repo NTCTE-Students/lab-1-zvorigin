9c69af967cfdb9ffde471e2e246e2dcaa21b02c9
Зворыгин Кирилл <zvorigin.kirill@nttek.ru>
2025-01-30 14:24:01 +0500

    Сделал 1 лабу.

---
+++ greetings.php
@@ -0,0 +1,9 @@
+< i DOCTYPE <html>
+<html lang="ru"
+<head>
+<body>
+<h1>Дружелюбный файл!<h1>
+<p>
+<form method="post">
+
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Первый код написан через Html</title>
+</head>
+<body>
+<?php
+    for ($i = 1; $i <= 10; $i++)
+print
+?>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="ru"
+<head>
+<meta charset="UTF-8">
+<meta name="viewport content="width=device-width, initial-scale=1.0">
+<title>Моя первая страница на PHP!</title>
+</HEAD>
+<BODY>
+<?php
+PRINT('Hello,world')
+?>
+<body>
+<html>
+</head>
+<body>
+$greetings = htmlspecialchars(string: $_port['name']);
+
+
