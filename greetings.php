b720fd93a1a9db1063e0a3a9048b4e50d1860027
zvorigin <zvorigin.kirill@nttek.ru>
2025-04-11 12:20:10 +0500

    Я сдал работу на 100%

--- greetings.php
+++ greetings.php
@@ -1,9 +1,16 @@
-< i DOCTYPE <html>
-<html lang="ru"
+<!DOCTYPE html>
+<html lang="ru">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Приветствие</title>
+</head>
 <body>
-<h1>Дружелюбный файл!<h1>
-<p>
-<form method="post">
-
+  <h1>Приветствие</h1>
+  <p>Просто файл, который хочет быть добродушным!</p>
+  <form method="POST">
+      <input type="text" name="name" placeholder="Введите ваше имя">
+      <input type="submit" value="Отправить">
+  </form>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,32 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="ru">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Первый код написан через Html</title>
-</head>
-<body>
-<?php
-    for ($i = 1; $i <= 10; $i++)
-print
-?>
-</body>
+    <title>Моя первая страница на PHP!</title>
+  </head>
+  <body>
+    <?php
+      print('Привет, Мир!');
+    ?>
+  </body>
 </html>
-<!DOCTYPE html>
-<html lang="ru"
-<head>
-<meta charset="UTF-8">
-<meta name="viewport content="width=device-width, initial-scale=1.0">
-<title>Моя первая страница на PHP!</title>
-</HEAD>
-<BODY>
-<?php
-PRINT('Hello,world')
-?>
-<body>
-<html>
-</head>
-<body>
-$greetings = htmlspecialchars(string: $_port['name']);
-
-
